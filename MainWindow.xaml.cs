863ff0d68b7298e58736c441ac313a19770b4973
Andrew Punnett <andrew.punnett@gmail.com>
2016-09-22 15:29:36 +1200

    Add a working application.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace d3js_in_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Debug.AutoFlush = true;
+            InitializeComponent();
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            webBrowser.NavigateToStream(System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("d3js_in_WPF.iris.html"));
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void iris_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            webBrowser.NavigateToStream(System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("d3js_in_WPF.iris.html"));
+        }
+
+        private void lesMiserables_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            webBrowser.NavigateToStream(System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("d3js_in_WPF.lesmiserables.html"));
+        }
+
+        private void worldTour__MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            webBrowser.NavigateToStream(System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("d3js_in_WPF.worldtour.html"));
+        }
+
+        private void collision__MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            webBrowser.NavigateToStream(System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("d3js_in_WPF.collision.html"));
+        }
+
+        private void viewHelp_Click(object sender, RoutedEventArgs e)
+        {
+            openAbout();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            openAbout();
+        }
+
+        private void openAbout()
+        {
+            var about = new AboutWindow();
+            about.ShowDialog();
+        }
+    }
+}
---
+++ MyModule1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+
+namespace d3js_in_WPF
+{
+    public class MyModule1 : IHttpModule
+    {
+        /// <summary>
+        /// You will need to configure this module in the Web.config file of your
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            //clean-up code here.
+        }
+
+        public void Init(HttpApplication context)
+        {
+            // Below is an example of how you can handle LogRequest event and provide 
+            // custom logging implementation for it
+            context.LogRequest += new EventHandler(OnLogRequest);
+        }
+
+        #endregion
+
+        public void OnLogRequest(Object source, EventArgs e)
+        {
+            //custom logging logic can go here
+        }
+    }
+}
